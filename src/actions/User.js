8a71ff5b63f1d35c572c56459960d2e2fcfb9569
Daniel Regala <daniel@doubletrips.io>
2017-08-17 17:13:44 +0800

    Updates

--- src/actions/User.js
+++
@@ -1,3 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const getUserList = createAction("GET_USER_LIST");
--- src/reducers/Users.js
+++
@@ -1,9 +0,0 @@
-import { handleActions } from 'redux-actions';
-import { userList as initialState} from '../data/generated';
-import { getUserList } from "../actions/User";
-
-export default handleActions({
-    [getUserList]: (state, action) => ({
-        state
-    })
-}, initialState)
